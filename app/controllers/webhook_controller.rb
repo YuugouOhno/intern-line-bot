913596c70b803c80223f357ac6721bd9788ac3e6
Yuugou Ohno <yuugouohno@YuugounoMacBook-Pro.local>
2022-10-20 14:54:39 +0900

    [update]返信するテキストの変更、コードのメソッド化

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -10,10 +10,33 @@ class WebhookController < ApplicationController
     }
   end
 
+  STICKER_PACKAGE_ID = 446
+  STICKER_ID_BEGIN = 1988
+  STICKER_ID_END = 2027
+
+  # ランダムなスタンプのIDを返す
+  def pickup_random_sticker_id
+    return rand(STICKER_ID_BEGIN..STICKER_ID_END)
+  end
+
+  POSITIVE_TEXT_LIST = %w[なるほど 確かに そうだね すごい 最高だね 天才だわ]
+  NEGATIVE_TEXT_LIST = %w[何言ってんの それはない いい加減にして？ そうかなぁ]
+  POLITE_TEXT_LIST = %w[仰る通りです その通りですね ごもっともですね 興味深いですね 全くもって同感です]
+
+  # 任意のテキストリストからランダムに取得する
+  def pickup_random_text(type)
+    case type
+    when 'positive'
+      return POSITIVE_TEXT_LIST.sample
+    when 'negative'
+      return NEGATIVE_TEXT_LIST.sample
+    when 'polite'
+      return POLITE_TEXT_LIST.sample
+    end
+  end
+
   def callback
     body = request.body.read
-    puts '-'*100
-    p request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       head 470
@@ -25,17 +48,16 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          reply_message_list = %w[test1 test2 test3 test4 test5]
           message = {
             type: 'text',
-            text: reply_message_list.sample
+            text: pickup_random_text('positive')
           }
           client.reply_message(event['replyToken'], message)
         else
           message = {
             type: "sticker",
-            packageId: "446",
-            stickerId: rand(1988..2027)
+            packageId: STICKER_PACKAGE_ID,
+            stickerId: pickup_random_sticker_id
           }
           client.reply_message(event['replyToken'], message)
         end
