ce661e96650988b70fcb181ca28fd7d246d3d7d4
Yuugou Ohno <yuugouohno@YuugounoMacBook-Pro.local>
2022-10-20 15:28:56 +0900

    [clean]private methodに変更

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,38 +3,6 @@ require 'line/bot'
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
-  STICKER_PACKAGE_ID = 446
-  STICKER_ID_BEGIN = 1988
-  STICKER_ID_END = 2027
-
-  # ランダムなスタンプのIDを返す
-  def pickup_random_sticker_id
-    return rand(STICKER_ID_BEGIN..STICKER_ID_END)
-  end
-
-  POSITIVE_TEXT_LIST = %w[なるほど 確かに そうだね すごい 最高だね 天才だわ]
-  NEGATIVE_TEXT_LIST = %w[何言ってんの それはない いい加減にして？ そうかなぁ]
-  POLITE_TEXT_LIST = %w[仰る通りです その通りですね ごもっともですね 興味深いですね 全くもって同感です]
-
-  # 任意のテキストリストからランダムに取得する
-  def pickup_random_text(type)
-    case type
-    when 'positive'
-      return POSITIVE_TEXT_LIST.sample
-    when 'negative'
-      return NEGATIVE_TEXT_LIST.sample
-    when 'polite'
-      return POLITE_TEXT_LIST.sample
-    end
-  end
-
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -65,4 +33,37 @@ class WebhookController < ApplicationController
     }
     head :ok
   end
+
+  private
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
+
+    STICKER_PACKAGE_ID = 446
+    STICKER_ID_BEGIN = 1988
+    STICKER_ID_END = 2027
+
+    # ランダムなスタンプのIDを返す
+    def pickup_random_sticker_id
+      return rand(STICKER_ID_BEGIN..STICKER_ID_END)
+    end
+
+    POSITIVE_TEXT_LIST = %w[なるほど 確かに そうだね すごい 最高だね 天才だわ]
+    NEGATIVE_TEXT_LIST = %w[何言ってんの それはない いい加減にして？ そうかなぁ]
+    POLITE_TEXT_LIST = %w[仰る通りです その通りですね ごもっともですね 興味深いですね 全くもって同感です]
+
+    # 任意のテキストリストからランダムに取得する
+    def pickup_random_text(type)
+      case type
+      when 'positive'
+        return POSITIVE_TEXT_LIST.sample
+      when 'negative'
+        return NEGATIVE_TEXT_LIST.sample
+      when 'polite'
+        return POLITE_TEXT_LIST.sample
+      end
+    end
 end
